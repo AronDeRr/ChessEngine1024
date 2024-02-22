472e4b8adb8bf6a3f23d2d6ad4adcf417b6236d6
Aron de Ruijter <aronder@live.nl>
2024-02-22 14:36:29 +0100

    Move evaluation based on piece value

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,9 +2,56 @@
 
 public class MyBot : IChessBot
 {
+
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = {0, 100, 300, 300, 500, 900, 10000};
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move bestMove = moves[0];
+        int bestScore = -10000;
+        int who2Move = board.IsWhiteToMove? 1: -1;
+
+        foreach (Move move in moves)
+        {
+            board.MakeMove(move);
+            int score = Evaluate(board, who2Move);
+            board.UndoMove(move);
+
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        return bestMove;
     }
+
+    private int Evaluate(Board board, int who2Move)
+    {
+        if (board.IsInCheckmate())
+        {
+            return int.MaxValue;
+        }
+        else if (board.IsDraw())
+        {
+            return 0;
+        }
+
+        int whitePoints = 0;
+        int blackPoints = 0;
+        for (int i = 0; i <= 63; i++)
+        {
+            Piece p = board.GetPiece(new Square(i));
+
+            if (p.IsWhite) whitePoints += pieceValues[(int)p.PieceType];
+            else blackPoints += pieceValues[(int)p.PieceType];
+        }
+
+        int score = (whitePoints - blackPoints) * who2Move;
+
+        return score;
+    }
+
 }
